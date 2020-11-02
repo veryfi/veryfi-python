2241516d5a348aa2f921b76498a12dd4d639d45e
manycoding <manycoding@users.noreply.github.com>
2020-11-02 13:17:01 -0300

    Make it black

--- veryfi/__init__.py
+++ veryfi/__init__.py
@@ -1,5 +1,5 @@
-__version_info__ = ('0', '0', '1')
-__version__ = '.'.join(__version_info__)
+__version_info__ = ("0", "0", "1")
+__version__ = ".".join(__version_info__)
 
 import base64
 import hashlib
@@ -18,21 +18,23 @@ class Client:
     API_TIMEOUT = 120
     MAX_FILE_SIZE_MB = 20
     BASE_URL = "https://api.veryfi.com/api/"
-    CATEGORIES = ["Advertising & Marketing",
-                  "Automotive",
-                  "Bank Charges & Fees",
-                  "Legal & Professional Services",
-                  "Insurance",
-                  "Meals & Entertainment",
-                  "Office Supplies & Software",
-                  "Taxes & Licenses",
-                  "Travel",
-                  "Rent & Lease",
-                  "Repairs & Maintenance",
-                  "Payroll",
-                  "Utilities",
-                  "Job Supplies",
-                  "Grocery"]
+    CATEGORIES = [
+        "Advertising & Marketing",
+        "Automotive",
+        "Bank Charges & Fees",
+        "Legal & Professional Services",
+        "Insurance",
+        "Meals & Entertainment",
+        "Office Supplies & Software",
+        "Taxes & Licenses",
+        "Travel",
+        "Rent & Lease",
+        "Repairs & Maintenance",
+        "Payroll",
+        "Utilities",
+        "Job Supplies",
+        "Grocery",
+    ]
 
     def __init__(
         self,
@@ -42,7 +44,7 @@ class Client:
         api_key=None,
         base_url=BASE_URL,
         api_version=API_VERSION,
-        timeout=API_TIMEOUT
+        timeout=API_TIMEOUT,
     ):
         self.client_id = client_id
         self.client_secret = client_secret
@@ -54,21 +56,23 @@ class Client:
         self.headers = {}
         self._session = requests.Session()
 
-    def _get_headers(self, has_files:bool=False):
+    def _get_headers(self, has_files: bool = False):
         """
         Prepares the headers needed for a request.
         :param has_files: Are there any files to be submitted as binary
         :return: Dictionary with headers
         """
         final_headers = {
-            "User-Agent": 'Python Veryfi-Python/0.1',
+            "User-Agent": "Python Veryfi-Python/0.1",
             "Accept": "application/json",
             "Content-Type": "application/json",
-            "Client-Id": self.client_id
+            "Client-Id": self.client_id,
         }
 
         if self.username:
-            final_headers.update({"Authorization": "apikey {}:{}".format(self.username, self.api_key)})
+            final_headers.update(
+                {"Authorization": "apikey {}:{}".format(self.username, self.api_key)}
+            )
 
         if has_files:
             final_headers.pop("Content-Type", "application/x-www-form-urlencoded")
@@ -98,11 +102,20 @@ class Client:
         if self.client_secret:
             timestamp = int(time.time() * 1000)
             signature = self._generate_signature(request_arguments, timestamp=timestamp)
-            headers.update({"X-Veryfi-Request-Timestamp": str(timestamp),
-                            "X-Veryfi-Request-Signature": signature})
+            headers.update(
+                {
+                    "X-Veryfi-Request-Timestamp": str(timestamp),
+                    "X-Veryfi-Request-Signature": signature,
+                }
+            )
 
-        response = self._session.request(http_verb, url=api_url, headers=headers, data=json.dumps(request_arguments),
-                                         timeout=self.timeout)
+        response = self._session.request(
+            http_verb,
+            url=api_url,
+            headers=headers,
+            data=json.dumps(request_arguments),
+            timeout=self.timeout,
+        )
 
         if response.status_code not in [200, 201, 202, 204]:
             raise VeryfiClientError.from_response(response)
@@ -121,10 +134,12 @@ class Client:
             value = payload_params[key]
             payload = "{0},{1}:{2}".format(payload, key, value)
 
-        secret_bytes = bytes(self.client_secret, 'utf-8')
-        payload_bytes = bytes(payload, 'utf-8')
-        tmp_signature = hmac.new(secret_bytes, msg=payload_bytes, digestmod=hashlib.sha256).digest()
-        base64_signature = base64.b64encode(tmp_signature).decode("utf-8") .strip()
+        secret_bytes = bytes(self.client_secret, "utf-8")
+        payload_bytes = bytes(payload, "utf-8")
+        tmp_signature = hmac.new(
+            secret_bytes, msg=payload_bytes, digestmod=hashlib.sha256
+        ).digest()
+        base64_signature = base64.b64encode(tmp_signature).decode("utf-8").strip()
         return base64_signature
 
     def get_documents(self):
@@ -134,9 +149,9 @@ class Client:
         """
         endpoint_name = "/documents/"
         request_arguments = {}
-        documents = self._request('GET', endpoint_name, request_arguments)
-        if 'documents' in documents:
-            return documents['documents']
+        documents = self._request("GET", endpoint_name, request_arguments)
+        if "documents" in documents:
+            return documents["documents"]
         return documents
 
     def get_document(self, document_id):
@@ -147,7 +162,7 @@ class Client:
         """
         endpoint_name = "/documents/{}/".format(document_id)
         request_arguments = {"id": document_id}
-        document = self._request('GET', endpoint_name, request_arguments)
+        document = self._request("GET", endpoint_name, request_arguments)
         return document
 
     def process_document(self, file_path, categories=None, delete_after_processing=False):
@@ -163,12 +178,14 @@ class Client:
             categories = self.CATEGORIES
         file_name = os.path.basename(file_path)
         with open(file_path, "rb") as image_file:
-            base64_encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
-        request_arguments = {"file_name": file_name,
-                             "file_data": base64_encoded_string,
-                             "categories": categories,
-                             "auto_delete": delete_after_processing}
-        document = self._request('POST', endpoint_name, request_arguments)
+            base64_encoded_string = base64.b64encode(image_file.read()).decode("utf-8")
+        request_arguments = {
+            "file_name": file_name,
+            "file_data": base64_encoded_string,
+            "categories": categories,
+            "auto_delete": delete_after_processing,
+        }
+        document = self._request("POST", endpoint_name, request_arguments)
         return document
 
     def _process_document_file(self, file_path, categories=None, delete_after_processing=False):
@@ -183,11 +200,13 @@ class Client:
         if not categories:
             categories = self.CATEGORIES
         file_name = os.path.basename(file_path)
-        request_arguments = {"file_name": file_name,
-                             "categories": categories,
-                             "auto_delete": delete_after_processing}
+        request_arguments = {
+            "file_name": file_name,
+            "categories": categories,
+            "auto_delete": delete_after_processing,
+        }
         with open(file_path) as file_stream:
-            document = self._request('POST', endpoint_name, request_arguments, file_stream)
+            document = self._request("POST", endpoint_name, request_arguments, file_stream)
         return document
 
     def process_document_url(self, file_url, categories=None, delete_after_processing=False):
@@ -199,11 +218,13 @@ class Client:
         :return: Data extracted from the document
         """
         endpoint_name = "/documents/"
-        request_arguments = {"file_url": file_url,
-                             "categories": categories,
-                             "auto_delete": delete_after_processing}
+        request_arguments = {
+            "file_url": file_url,
+            "categories": categories,
+            "auto_delete": delete_after_processing,
+        }
 
-        return self._request('POST', endpoint_name, request_arguments)
+        return self._request("POST", endpoint_name, request_arguments)
 
     def delete_document(self, document_id):
         """
@@ -212,4 +233,4 @@ class Client:
         """
         endpoint_name = "/documents/{0}/".format(document_id)
         request_arguments = {"id": document_id}
-        self._request('DELETE', endpoint_name, request_arguments)
+        self._request("DELETE", endpoint_name, request_arguments)
