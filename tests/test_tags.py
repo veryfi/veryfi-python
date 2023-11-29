11d1ea6f6422089d2a4bf94572db67c27a7306b0
Alejandro Uribe <alejouribesanchez@gmail.com>
2023-11-28 23:40:09 -0600

    Update test_tags.py

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -3,14 +3,12 @@ import responses
 
 from veryfi import *
 
+
 @pytest.mark.parametrize("client_secret", [None, "s"])
 @responses.activate
 def test_tags(client_secret):
     mock_doc_id = 169985445
-    mock_resp = {
-    "id": 6673474,
-    "name": "tag_123"
-    }
+    mock_resp = {"id": 6673474, "name": "tag_123"}
     client = Client(client_id="v", client_secret=client_secret, username="o", api_key="c")
     responses.add(
         responses.PUT,
@@ -18,7 +16,4 @@ def test_tags(client_secret):
         json=mock_resp,
         status=200,
     )
-    assert (
-        client.add_tag(mock_doc_id, "tag_123")
-        == mock_resp
-    )
+    assert client.add_tag(mock_doc_id, "tag_123") == mock_resp
