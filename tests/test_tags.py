1124e5149067c7ff68ceab83589ed5a1dc607072
Alejandro Uribe <alejouribesanchez@gmail.com>
2023-11-29 09:48:55 -0600

    Update test_tags.py

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -10,8 +10,7 @@ def test_tags(client_secret):
     mock_doc_id = 169985445
     mock_resp = {"id": 6673474, "name": "tag_123"}
     client = Client(client_id="v", client_secret=client_secret, username="o", api_key="c")
-    responses.add(
-        responses.PUT,
+    responses.put(
         f"{client.versioned_url}/partner/documents/{mock_doc_id}/tags/",
         json=mock_resp,
         status=200,
